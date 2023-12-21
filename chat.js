33e9059762194be29743c12d8d38d0c77057ecca
linteresting <lwtmax@gmail.com>
2023-12-21 20:13:03 +0800

    chat err

--- chat.js
+++ chat.js
@@ -1,5 +1,4 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
-import fetch from 'node-fetch';
 
 let model = null;
 
@@ -21,8 +20,8 @@ export async function chat(prompt) {
     const response = await result.response;
     return response.text(); // 或者 response.json()，取决于响应格式
   } catch (error) {
-    // console.error("Error in chat function:", error);
-    throw error; // 或者返回一个错误信息
+    console.error("Error in chat function:", error);
+    return "获取res错误";
   }
 }
 
